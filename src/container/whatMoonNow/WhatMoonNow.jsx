2d7ad82258250682f3730f536fe1afd3238111c6
Christian Yugen <j.yugen@yahoo.com>
2023-02-26 20:56:38 -0300

    Criando pastas de arquivo react.js

---
+++ src/container/whatMoonNow/WhatMoonNow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatMoonNow.css';
+
+const WhatMoonNow = () => {
+    return(
+        <div>
+            WhatMoonNow
+        </div>
+    )
+}
+
+export default WhatMoonNow
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'; //isso vai permetir usar o react.js no index.html
 import ReactDom from 'react-dom';
 
+import App from './App';
+
 ReactDom.render(<App />, document.getElementById('root'));
 
